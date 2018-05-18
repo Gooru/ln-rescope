f10278131fc5a95931604be557f19891c0723ab0
am-gooru <ashish@gooru.org>
2018-05-18 14:16:41 +0530

    Persistence storage schema for the queue

--- sql/rescope.sql
+++ sql/rescope.sql
@@ -1,4 +1,5 @@
 -- drop table user_rescoped_content
+-- drop table rescope_queue
 
 create table user_rescoped_content (
     id bigserial NOT NULL,
@@ -21,4 +22,28 @@ CREATE UNIQUE INDEX urc_ucc_unq_idx
     where class_id is null;
 
 COMMENT on table user_rescoped_content IS 'Store the rescoped content for user/course/class combination';
+
+create table rescope_queue (
+    id bigserial NOT NULL,
+    user_id uuid NOT NULL,
+    course_id uuid NOT NULL,
+    class_id uuid,
+    status int NOT NULL DEFAULT 0 CHECK (status::int = ANY (ARRAY[0::int, 1::int, 2::int])),
+    created_at timestamp without time zone DEFAULT timezone('UTC'::text, now()) NOT NULL,
+    updated_at timestamp without time zone DEFAULT timezone('UTC'::text, now()) NOT NULL,
+    CONSTRAINT rq_pkey PRIMARY KEY (id)
+);
+
+ALTER TABLE rescope_queue OWNER TO nucleus;
+
+CREATE UNIQUE INDEX rq_ucc_null_unq_idx
+    ON rescope_queue (user_id, course_id, class_id)
+    where class_id is not null;
+
+CREATE UNIQUE INDEX rq_ucc_unq_idx
+    ON rescope_queue (user_id, course_id)
+    where class_id is null;
+
+COMMENT on TABLE rescope_queue IS 'Persistent queue for rescope tasks';
+COMMENT on COLUMN rescope_queue.status IS '0 means queued, 1 means dispatched for processing, 2 means in process'
 
