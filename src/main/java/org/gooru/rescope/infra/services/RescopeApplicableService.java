f3ada2dc8b848d5ce2c8513488117aa8d0558266
am-gooru <ashish@gooru.org>
2018-07-09 12:20:05 +0530

    Rescope applicability is governed by course version from config file. The value in config file is 'premium'.

--- src/main/java/org/gooru/rescope/infra/services/RescopeApplicableService.java
+++ src/main/java/org/gooru/rescope/infra/services/RescopeApplicableService.java
@@ -2,7 +2,10 @@ package org.gooru.rescope.infra.services;
 
 import java.util.UUID;
 
+import org.gooru.rescope.infra.components.AppConfiguration;
 import org.gooru.rescope.infra.jdbi.DBICreator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import io.vertx.core.json.JsonObject;
 
@@ -11,21 +14,23 @@ import io.vertx.core.json.JsonObject;
  */
 public final class RescopeApplicableService {
 
-    private static final String RESCOPE_SETTING_KEY = "rescope";
+    private static final Logger LOGGER = LoggerFactory.getLogger(RescopeApplicableService.class);
 
     public static boolean isRescopeApplicableToClass(UUID classId) {
         RescopeApplicableDao dao = getRescopeApplicableDao();
-        String setting = dao.fetchClassSetting(classId);
-        if (setting == null) {
-            throw new IllegalStateException("Class setting should not be null");
+        String courseId = dao.fetchCourseForClass(classId);
+        if (courseId == null) {
+            LOGGER.info("Course is not assigned to class '{}' hence rescope not applicable", classId.toString());
+            return false;
         }
-        JsonObject jsonSetting = new JsonObject(setting);
-        return Boolean.TRUE.equals(jsonSetting.getBoolean(RESCOPE_SETTING_KEY));
+        return AppConfiguration.getInstance().getRescopeApplicableCourseVersion()
+            .equals(dao.fetchCourseVersion(courseId));
     }
 
     public static boolean isRescopeApplicableToCourseInIL(UUID courseId) {
         RescopeApplicableDao dao = getRescopeApplicableDao();
-        return (dao.fetchCourseVersion(courseId) != null);
+        return AppConfiguration.getInstance().getRescopeApplicableCourseVersion()
+            .equals(dao.fetchCourseVersion(courseId));
     }
 
     private static RescopeApplicableDao getRescopeApplicableDao() {
